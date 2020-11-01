b8434ea220bc6b26b378b23b0e73e30127edc4d8
arnauph <arnau@boka2.com>
2020-11-01 22:14:41 +0100

    Pujar Imatges amb Preview

--- app/src/main/java/com/boka2/gesblue/activities/PujarImatges.java
+++ app/src/main/java/com/boka2/gesblue/activities/PujarImatges.java
@@ -1,10 +1,17 @@
 package com.boka2.gesblue.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.boka2.gesblue.R;
 import com.boka2.gesblue.adapters.DenunciaAdapter;
@@ -23,15 +30,26 @@ import pt.joaocruz04.lib.misc.JSoapCallback;
 
 public class PujarImatges extends AppCompatActivity {
 
+    public static RecyclerView llistat;
+    public static ProgressBar loading;
+    public static Context mContext;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pujar_imatges);
 
+        mContext=this;
+        llistat= this.findViewById(R.id.recycler);
+        loading=this.findViewById(R.id.loading_bar);
+        loading.setVisibility(View.GONE);
+        loading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        RecyclerView llistat= this.findViewById(R.id.recycler);
-
-
+            }
+        });
         File path = new File("storage/emulated/0/Boka2/upload/error");
 
         if(path.exists()) {
@@ -39,22 +57,83 @@ public class PujarImatges extends AppCompatActivity {
             final File[] files = path.listFiles();
             int i=0;
 
+
+            LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+            llistat.setLayoutManager(mLayoutManager);
+
+
             // specify an adapter (see also next example)
             PujarFotos_Adapter mAdapter = new PujarFotos_Adapter(this,files);
             llistat.setAdapter(mAdapter);
-
-
-
-
-
         }
 
 
 
+    }
+
+    public static void Refresh(){
+        File path = new File("storage/emulated/0/Boka2/upload/error");
 
 
+        if(path.exists()) {
+            String[] fileNames = path.list();
+            final File[] files = path.listFiles();
+            int i=0;
 
 
+            LinearLayoutManager mLayoutManager = new LinearLayoutManager(mContext);
+            llistat.setLayoutManager(mLayoutManager);
+
+
+            // specify an adapter (see also next example)
+            PujarFotos_Adapter mAdapter = new PujarFotos_Adapter(mContext,files);
+            llistat.setAdapter(mAdapter);
+
+            StopLoading();
+        }
+        else{
+            StopLoading();
+        }
+    }
+
+    public static void StartLoading(){
+        loading.setVisibility(View.VISIBLE);
+    }
+
+    public static void StopLoading(){
+        loading.setVisibility(View.GONE);
+    }
+
+
+    public static void Message(Integer Result){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(mContext);
+
+        if(Result==0){
+            alertDialogBuilder.setTitle(mContext.getString(R.string.atencio))
+                    .setMessage(mContext.getString(R.string.sendFoto_failed_title))
+                    .setPositiveButton(mContext.getString(R.string.ok), new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+
+                        }
+                    });
+
+            AlertDialog alert = alertDialogBuilder.create();
+            alert.show();
+        }
+        else if(Result==1){
+            alertDialogBuilder.setTitle(mContext.getString(R.string.atencio))
+                    .setMessage(mContext.getString(R.string.sendFoto_succesfull_title))
+                    .setPositiveButton(mContext.getString(R.string.ok), new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+
+                        }
+                    });
+
+            AlertDialog alert = alertDialogBuilder.create();
+            alert.show();
+        }
 
 
     }
--- app/src/main/java/com/boka2/gesblue/adapters/PujarFotos_Adapter.java
+++ app/src/main/java/com/boka2/gesblue/adapters/PujarFotos_Adapter.java
@@ -3,24 +3,35 @@ package com.boka2.gesblue.adapters;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.boka2.gesblue.R;
+import com.boka2.gesblue.activities.PujarImatges;
+import com.boka2.gesblue.datamanager.webservices.DatamanagerAPI;
+import com.boka2.gesblue.datamanager.webservices.requests.operativa.PujaFotoRequest;
+import com.boka2.gesblue.global.PreferencesGesblue;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.net.util.Base64;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /*
  * Created by Boka2.
  */
-;
+;import pt.joaocruz04.lib.misc.JSoapCallback;
 
 public class PujarFotos_Adapter extends RecyclerView.Adapter<PujarFotos_Adapter.PujaFotoViewHolder>  {
 
@@ -46,13 +57,47 @@ public class PujarFotos_Adapter extends RecyclerView.Adapter<PujarFotos_Adapter.
     @Override
     public void onBindViewHolder(PujaFotoViewHolder holder, final int position) {
         //getting the denuncia of the specified position
-        File foto = Files[position];
+        final File foto = Files[position];
 
-        //binding the data with the viewholder views
-        Bitmap bitmap = BitmapFactory.decodeFile(foto.getPath());
-        holder.img_preview.setImageBitmap(bitmap);
 
-        holder.text_nom.setText(foto.getName());
+        String[] valors=foto.getName().split("-");
+        if(valors.length>2) {
+            String nom=valors[2].substring(0,(valors[2].length()-4));
+
+            holder.text_nom.setText(nom.substring(0,nom.length()-1));
+            holder.text_numfoto.setText(nom.substring(nom.length()-1));
+
+
+            SimpleDateFormat dateFormatter =new SimpleDateFormat("yyyyMMddHHmmss");
+            SimpleDateFormat dateFormatterResultant =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                Date data= dateFormatter.parse(valors[0]);
+
+                holder.text_data.setText(dateFormatterResultant.format(data));
+            } catch (ParseException e) {
+                holder.text_data.setText(valors[0]);
+            }
+
+            //binding the data with the viewholder views
+            Bitmap bitmap = BitmapFactory.decodeFile(foto.getPath());
+            holder.img_preview.setImageBitmap(bitmap);
+
+
+            holder.lay_base.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    PujarFoto(foto,mCtx);
+
+                }
+            });
+        }
+        else{
+            holder.text_numfoto.setText("Te un format no corresponent");
+            holder.text_data.setVisibility(View.GONE);
+        }
+
+
 
 
 
@@ -67,17 +112,71 @@ public class PujarFotos_Adapter extends RecyclerView.Adapter<PujarFotos_Adapter.
 
     class PujaFotoViewHolder extends RecyclerView.ViewHolder {
 
-        TextView text_nom;
+        TextView text_nom,text_data,text_numfoto;
         ImageView img_preview;
+        ConstraintLayout lay_base;
         public PujaFotoViewHolder(View itemView) {
             super(itemView);
-            text_nom = (TextView) itemView.findViewById(R.id.txt_name);
+            text_nom = (TextView) itemView.findViewById(R.id.txt_numdenucnia);
+            text_data=(TextView) itemView.findViewById(R.id.txt_data);
+            text_numfoto=(TextView) itemView.findViewById(R.id.txt_numfoto);
             img_preview = (ImageView) itemView.findViewById(R.id.img_preview);
+            lay_base=(ConstraintLayout) itemView.findViewById(R.id.lay_base);
             //itemView.setOnClickListener(this);
         }
         /*@Override
         public void onClick(View view) {
             Log.d("Miquel", "onClick " + getPosition());
         }*/
+    }
+
+
+
+    private void PujarFoto(final File file, Context aContext){
+        PujarImatges.StartLoading();
+        try {
+            byte[] encoded = Base64.encodeBase64(FileUtils.readFileToByteArray(file));
+            String str_encoded = new String(encoded, StandardCharsets.US_ASCII);
+
+
+            PujaFotoRequest pjr = new PujaFotoRequest(
+                    PreferencesGesblue.getConcessio(aContext),
+                    str_encoded,
+                    file.getName()
+            );
+            DatamanagerAPI.crida_PujaFoto(pjr,
+                    new JSoapCallback() {
+                        @Override
+                        public void onSuccess(String result) {
+                            File direct = new File("storage/emulated/0/Boka2/upload/done");
+
+                            if (!direct.exists()) {
+                                File wallpaperDirectory = new File("storage/emulated/0/Boka2/upload/done");
+                                wallpaperDirectory.mkdirs();
+                            }
+                            File from = new File("storage/emulated/0/Boka2/upload/error/" + file.getName());
+                            File to = new File("storage/emulated/0/Boka2/upload/done/" + file.getName());
+                            from.renameTo(to);
+
+
+                            PujarImatges.Refresh();
+                            PujarImatges.Message(1);
+                        }
+
+                        @Override
+                        public void onError(int error) {
+                            Log.e("Formulari", "Error PujaFoto: " + error);
+                            PujarImatges.StopLoading();
+                            PujarImatges.Message(0);
+
+                        }
+                    });
+
+        }
+        catch (Exception e) {
+            PujarImatges.StopLoading();
+            PujarImatges.Message(0);
+            e.printStackTrace();
+        }
     }
 }
