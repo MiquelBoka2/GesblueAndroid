cf7548d47720c6b7119688f392f4ffa8bf1547f3
arnauph <arnau@boka2.com>
2021-11-29 20:38:24 +0100

    1.6.8 HTTPS

--- app/src/main/java/com/boka2/gesblue/global/Constants.java
+++ app/src/main/java/com/boka2/gesblue/global/Constants.java
@@ -30,11 +30,11 @@ public class Constants {
     // ==================================
     // DATAMANAGER
     // ==================================
-    public static final String DADESBA_URL = "http://www.gesblue.com/app/v2/dadesbasiques.php";
+    public static final String DADESBA_URL = "https://www.gesblue.com/app/v2/dadesbasiques.php";
     public static final String DADESBA_NAMESPACE = WSConstantsGB.NAMESPACE_DADES_BASIQUES;
 
 
-    public static final String OPERATIVA_URL = "http://www.gesblue.com/app/v2/operativa.php";
+    public static final String OPERATIVA_URL = "https://www.gesblue.com/app/v2/operativa.php";
     public static final String OPERATIVA_NAMESPACE = WSConstantsGB.NAMESPACE_OPERATIVA;
 
     public static final String HASH = "#";
